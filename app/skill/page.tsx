55adec14486838e50122728a9fffcb6f24d2b1b6
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:42:23 +0700

    Upload app/skill/page.tsx

---
+++ app/skill/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/layout/PageLayout';
+import SkillsSection from '@/components/sections/SkillsSection';
+
+export default function SkillPage() {
+  return (
+    <PageLayout>
+      <SkillsSection />
+    </PageLayout>
+  );
+}
