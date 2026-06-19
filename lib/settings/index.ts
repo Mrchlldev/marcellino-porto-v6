d7e3a298933015d3c796e37fd3d29d60c9d0a787
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:42:51 +0700

    Upload lib/settings/index.ts

---
+++ lib/settings/index.ts
@@ -0,0 +1,7 @@
+export { siteConfig } from './site';
+export { heroConfig } from './hero';
+export { aboutConfig } from './about';
+export { skillsConfig } from './skills';
+export { educationConfig } from './education';
+export { projectsConfig } from './projects';
+export { contactConfig } from './contact';
