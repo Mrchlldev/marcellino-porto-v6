9b7e1caf2d65e84d4b2ea590667973e164f5f578
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:42:12 +0700

    Upload app/education/page.tsx

---
+++ app/education/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/layout/PageLayout';
+import EducationSection from '@/components/sections/EducationSection';
+
+export default function EducationPage() {
+  return (
+    <PageLayout>
+      <EducationSection />
+    </PageLayout>
+  );
+}
