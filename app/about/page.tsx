b6d4dc01cb974e7a9de07490c0baf1ef6b90aca2
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:42:08 +0700

    Upload app/about/page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/layout/PageLayout';
+import AboutSection from '@/components/sections/AboutSection';
+
+export default function AboutPage() {
+  return (
+    <PageLayout>
+      <AboutSection />
+    </PageLayout>
+  );
+}
