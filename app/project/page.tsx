b4ba92f42d537e80807c0abb49e161b06759d68b
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:42:21 +0700

    Upload app/project/page.tsx

---
+++ app/project/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/layout/PageLayout';
+import ProjectsSection from '@/components/sections/ProjectsSection';
+
+export default function ProjectPage() {
+  return (
+    <PageLayout>
+      <ProjectsSection />
+    </PageLayout>
+  );
+}
